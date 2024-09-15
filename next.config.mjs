3af56327edc314c942814c57b5923a466d960da4
Rafo <relax1199@mail.ru>
2024-09-15 10:45:16 +0400

    Change with back

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['127.0.0.1'], // Allow images from localhost
+        domains: ['berd.dahk.am'],
     },
     env: {
-        API_URL: "http://127.0.0.1:8000/api",
-        IMAGE_URL: "http://127.0.0.1:8000/storage/",
-        IMAGE_URL2: "http://127.0.0.1:8000/",
+        API_URL: "https://berd.dahk.am/api",
+        IMAGE_URL: "https://berd.dahk.am/storage/",
+        IMAGE_URL2: "https://berd.dahk.am/",
     },
 };
 
